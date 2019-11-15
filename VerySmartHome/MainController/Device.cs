66589cb782d57ae094cdd8f036f2e2e29e2f5c8f
Mohavk <mohavkv12@gmail.com>
2019-11-15 21:35:55 +0200

    Design. add MainController layer MainController folder has been added. A few main entities was created such as MainController, abstract DeviceController and Device

---
+++ VerySmartHome/MainController/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerySmartHome.MainController
+{
+    abstract class Device
+    {
+        protected abstract int ID { get; set; }
+        protected abstract int IP { get; set; }
+        protected abstract int Port { get; set; }
+    }
+}
---
+++ VerySmartHome/MainController/DeviceController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerySmartHome.MainController
+{
+    abstract class DeviceController
+    {
+        protected abstract IEnumerable<Device> Devices { get; set; }
+    }
+}
