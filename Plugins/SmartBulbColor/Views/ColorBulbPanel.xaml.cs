43152757e56420f2b5f8e4d34dafdfe61ce74ddb
Mohavk <mohavkv12@gmail.com>
2020-04-19 22:40:27 +0200

    Fix. Porting related bugs. Integrate resources. & Chore

--- Plugins/SmartBulbColor/Views/ColorBulbPanel.xaml.cs
+++ Plugins/SmartBulbColor/Views/ColorBulbPanel.xaml.cs
@@ -11,56 +11,9 @@ namespace SmartBulbColor.Views
     /// </summary>
     public partial class ColorBulbPanel : UserControl
     {
-        ImagePixelColorPicker ImageColorPicker = new ImagePixelColorPicker("Resources/ColorPicker.jpg");
         public ColorBulbPanel()
         {
             InitializeComponent();
-        }
-        private void Palette_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
-            {
-                var position = e.GetPosition(Palette);
-
-                var Width = ImageColorPicker.GetImageWidth();
-                var Height = ImageColorPicker.GetImageHeight();
-
-                Canvas.SetLeft(PickerMarker, position.X - 10);
-                Canvas.SetTop(PickerMarker, position.Y - 10);
-
-                double x = position.X * (Width / Palette.ActualWidth);
-                double y = position.Y * (Height / Palette.ActualHeight);
-
-                if (x < 0) x = 0;
-                if (x >= Width) x = Width - 1;
-                if (y < 0) y = 0;
-                if (y >= Height) y = Height - 1;
-
-                var color = ImageColorPicker.GetMediaColor((int)x, (int)y);
-                PickedColor.Fill = new SolidColorBrush(color);
-                Thread.Sleep(30);
-            }
-        }
-        private void Palette_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            var position = e.GetPosition(Palette);
-
-            var Width = ImageColorPicker.GetImageWidth();
-            var Height = ImageColorPicker.GetImageHeight();
-
-            Canvas.SetLeft(PickerMarker, position.X - 10);
-            Canvas.SetTop(PickerMarker, position.Y - 10);
-
-            double x = position.X * (Width / Palette.ActualWidth);
-            double y = position.Y * (Height / Palette.ActualHeight);
-
-            if (x < 0) x = 0;
-            if (x >= Width) x = Width - 1;
-            if (y < 0) y = 0;
-            if (y >= Height) y = Height - 1;
-
-            var color = ImageColorPicker.GetMediaColor((int)x, (int)y);
-            PickedColor.Fill = new SolidColorBrush(color);
         }
     }
 }
---
+++ Plugins/SmartBulbColor/Views/ColorPicker.xaml.cs
@@ -0,0 +1,95 @@
+﻿using SmartBulbColor.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartBulbColor.Views
+{
+    /// <summary>
+    /// Interaction logic for ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl
+    {
+        public static readonly DependencyProperty MyPropertyProperty;
+
+        static ColorPicker()
+        {
+            MyPropertyProperty = 
+                DependencyProperty.Register("SelectedColor", typeof(SolidColorBrush), typeof(ColorPicker));
+        }
+
+        public SolidColorBrush SelectedColor
+        {
+            get { return (SolidColorBrush)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        readonly Bitmap ColorPickerImage = Properties.Resources.ColorPicker;
+
+        public ColorPicker()
+        {
+            InitializeComponent();
+        }
+        private void Palette_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                var position = e.GetPosition(Palette);
+
+                var Width = ColorPickerImage.Width;
+                var Height = ColorPickerImage.Height;
+
+                Canvas.SetLeft(PickerMarker, position.X - 10);
+                Canvas.SetTop(PickerMarker, position.Y - 10);
+
+                double x = position.X * (Width / Palette.ActualWidth);
+                double y = position.Y * (Height / Palette.ActualHeight);
+
+                if (x < 0) x = 0;
+                if (x >= Width) x = Width - 1;
+                if (y < 0) y = 0;
+                if (y >= Height) y = Height - 1;
+
+                var pixel = ColorPickerImage.GetPixel((int)x, (int)y);
+                var color = HSBColor.DrowingToMediaColor(pixel);
+                SelectedColor = new SolidColorBrush(color);
+                ColorMarker.Fill = SelectedColor;
+                Thread.Sleep(30);
+            }
+        }
+        private void Palette_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var position = e.GetPosition(Palette);
+
+            var Width = ColorPickerImage.Width;
+            var Height = ColorPickerImage.Height;
+
+            Canvas.SetLeft(PickerMarker, position.X - 10);
+            Canvas.SetTop(PickerMarker, position.Y - 10);
+
+            double x = position.X * (Width / Palette.ActualWidth);
+            double y = position.Y * (Height / Palette.ActualHeight);
+
+            if (x < 0) x = 0;
+            if (x >= Width) x = Width - 1;
+            if (y < 0) y = 0;
+            if (y >= Height) y = Height - 1;
+
+            var pixel = ColorPickerImage.GetPixel((int)x, (int)y);
+            var color = HSBColor.DrowingToMediaColor(pixel);
+            SelectedColor = new SolidColorBrush(color);
+            ColorMarker.Fill = SelectedColor;
+        }
+    }
+}
